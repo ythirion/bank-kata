5957a577c15ebff46689c0b7bf6500c8713fdb0d
Yoan Thirion <yoan.thirion@gmail.com>
2022-08-23 16:28:25 +0200

    refactor Account

--- kata/src/main/scala/banking/domain/Account.scala
+++ kata/src/main/scala/banking/domain/Account.scala
@@ -3,16 +3,26 @@ package banking.domain
 import java.util.UUID
 
 case class Account(id: UUID, transactions: List[Transaction] = List()) {
-  def deposit(clock: Clock, amount: Double): Either[String, Account] = {
-    if (amount <= 0) Left("Invalid amount for deposit")
-    else
+  def deposit(clock: Clock, amount: Double): Either[String, Account] =
+    validateAmount(amount, "Invalid amount for deposit") { validAmount =>
       Right(
-        copy(transactions = Transaction(clock.now(), amount) :: transactions)
+        copy(transactions =
+          Transaction(clock.now(), validAmount) :: transactions
+        )
       )
-  }
+    }
 
   def withdraw(amount: Double): Either[String, Account] = {
-    if (amount <= 0) Left("Invalid amount for withdraw")
-    else Left(s"Not enough money to withdraw $amount")
+    validateAmount(amount, "Invalid amount for withdraw") { validAmount =>
+      Left(s"Not enough money to withdraw $validAmount")
+    }
   }
+
+  private def validateAmount(amount: Double, invalidAmountMessage: String)(
+      onValidAmount: Double => Either[String, Account]
+  ): Either[String, Account] =
+    if (isValidAmount(amount)) onValidAmount(amount)
+    else Left(invalidAmountMessage)
+
+  private def isValidAmount(amount: Double) = amount > 0
 }
